e854887d4f01b85d2698ed2d464a9aa4f36dab72
Junjie Jiang <junjiejiang94@gmail.com>
2017-03-10 13:24:00 -0800

    initial commit'

---
+++ index.ios.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, View, TextInput, TouchableOpacity, NavigatorIOS, ListView, Alert, AsyncStorage, Image } from 'react-native';
+import Style from "./Style";
+import SocketIOClient from 'socket.io-client';
+
+export default class BetaVuew extends Component {
+  constructor(props) {
+    super(props);
+    this.socket = SocketIOClient('http://localhost:8080', {
+      transports: ['websocket']
+    })
+  }
+  render() {
+    return (
+      <View style={ styles.container }>
+        <Text style={ styles.welcome }>
+          Welcome to React Native!
+        </Text>
+        <Text style={ styles.instructions }>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={ styles.instructions }>
+          Press Cmd+R to reload,
+          { '\n' } Cmd+D or shake for dev menu
+        </Text>
+      </View>
+      );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('BetaVuew', () => BetaVuew);
