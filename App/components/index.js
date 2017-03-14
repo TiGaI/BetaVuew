3908536d5b4f23531c8393d8a77d712fad6f7fad
Eric Abenojar <abenoeric@gmail.com>
2017-03-13 18:17:35 -0700

    added style to components

--- App/components/index.js
+++ App/components/index.js
@@ -1,6 +1,7 @@
 
 import React, { Component, PropTypes } from 'react';
 import { AppRegistry, ScrollView, StyleSheet, Text, View, TextInput, NavigatorIOS, TouchableOpacity, ListView, Alert, AsyncStorage, Image } from 'react-native';
+import { Container, Content, Left, Body, Right, ListItem, Thumbnail } from 'native-base';
 import Swiper from 'react-native-swiper'
 import randomcolor from 'randomcolor'
 
@@ -110,11 +111,43 @@ endReached(){
 }
 render() {
     return(
-      <View style={{flex:1}}>
+      <View style={{flex:1, padding: 10}}>
         <View style={{flex:1}}>
         <ScrollView style={{flex:1}}>
         <View style={styles.container2}>
-        <Text style={styles.title}>Sports</Text>
+        <Text style={{fontWeight: '200', fontSize: 20}}>Sports</Text>
+        <ListView
+        dataSource = {this.state.datafav}
+        renderRow={(val) =>
+          <TouchableOpacity onPress={this.press.bind(this, val)}>
+          <Image source={val.image} resizeMode="stretch" style={{width:330, height:220, margin:5,marginBottom:30, justifyContent:'center', alignItems:'center'}}>
+          <Text style={{backgroundColor:'rgba(0,0,0,0)', textAlign:'center', color:'#fff', fontSize:25, fontWeight:'700'}}>{val.name}</Text>
+          <Text style={{backgroundColor:'rgba(0,0,0,0)', color:'#fff',fontSize:13, fontWeight:'600'}}>{val.homes} homes</Text>
+          </Image>
+          </TouchableOpacity>
+        }
+        horizontal = {true}
+        showsHorizontalScrollIndicator = {false}
+        onEndReachedThreshold = {500}
+        onEndReached={this.endReached.bind(this)}
+        />
+        <Text style={{fontWeight: '200', fontSize: 20}}>Art</Text>
+        <ListView
+        dataSource = {this.state.datafav}
+        renderRow={(val) =>
+          <TouchableOpacity onPress={this.press.bind(this, val)}>
+          <Image source={val.image} resizeMode="stretch" style={{width:330, height:220, margin:5,marginBottom:30, justifyContent:'center', alignItems:'center'}}>
+          <Text style={{backgroundColor:'rgba(0,0,0,0)', textAlign:'center', color:'#fff', fontSize:25, fontWeight:'700'}}>{val.name}</Text>
+          <Text style={{backgroundColor:'rgba(0,0,0,0)', color:'#fff',fontSize:13, fontWeight:'600'}}>{val.homes} homes</Text>
+          </Image>
+          </TouchableOpacity>
+        }
+        horizontal = {true}
+        showsHorizontalScrollIndicator = {false}
+        onEndReachedThreshold = {500}
+        onEndReached={this.endReached.bind(this)}
+        />
+        <Text style={{fontWeight: '200', fontSize: 20}}>Food</Text>
         <ListView
         dataSource = {this.state.datafav}
         renderRow={(val) =>
@@ -147,9 +180,9 @@ class Swipe extends Component{
   viewStyle() {
     return {
       flex: 1,
-      backgroundColor: randomcolor(),
-      justifyContent: 'center',
-      alignItems: 'center',
+      backgroundColor: 'white',
+      justifyContent: 'flex-start',
+      padding: 15
     }
   }
   render(){
@@ -160,19 +193,56 @@ class Swipe extends Component{
         showsPagination={false}
         index={1}>
         <View style={this.viewStyle()}>
-          <TitleText label={this.props.name} />
+          <Text style={{fontWeight: '300', fontSize: 30, marginTop: 20, color: 'black'}}>Profile</Text>
+          <ListItem avatar>
+                        <Left>
+                            <Thumbnail source={require("../../assets/images/cyclist.jpg")} />
+                        </Left>
+                        <Body>
+                            <Text style={{fontWeight: '300', fontSize: 18}}>Junjie Jiang</Text>
+                            <Text note style={{fontWeight: '100', fontSize: 15}}>Doing what you like will always keep you happy . .</Text>
+                        </Body>
+                        <Right>
+                        </Right>
+                    </ListItem>
+          <View style={{flex: 1, borderBottomColor: 'grey', borderBottomWidth: 1, borderStyle: 'solid'}}>
+
+            <Text style={{fontSize: 18, fontWeight: '500', color: '#4E4E4E', marginTop: 10}}>Event Name {this.props.name}</Text>
+            <Text style={{fontSize: 13, fontWeight: '200', marginTop: 20}}>Retro venmo seitan, la croix before they sold out cronut hell of paleo lomo
+            post-ironic organic readymade chillwave salvia try-hard. Mlkshk meditation chambray,
+             celiac pop-up letterpress art party sartorial put a bird on it meh prism crucifix disrupt kickstarter. </Text>
+          </View>
+
+
         </View>
         <Swiper
           horizontal={false}
           loop={false}
           showsPagination={false}
           index={1}>
-          <View style={this.viewStyle()}>
-            <TitleText label={this.props.image} />
+          <View style={{flex: 1, backgroundColor: 'transparent'}}>
+            <Image source={require("../../assets/images/cyclist.jpg")}
+              resizeMode = "stretch"
+              style={{flex:3, alignItems:'center', width:null, height:null, justifyContent:'center'}}>
+            </Image>
+            <View style={{flex: 1, padding: 20}}>
+              <Text style={{flex: 1, fontSize: 25, fontWeight: '600', color: '#4E4E4E'}}>Cycling Mt.Awesome {this.props.homes}</Text>
+              <Text style={{flex: 1, fontSize: 18, fontWeight: '500', color: '#4E4E4E'}}>About the event</Text>
+              <Text style={{fontSize: 15, fontWeight: '400', color: '#656565'}}>Retro venmo seitan, la croix before they sold out cronut hell of paleo lomo
+              post-ironic organic readymade chillwave salvia try-hard. Mlkshk meditation chambray,
+               celiac pop-up letterpress art party </Text>
+            </View>
           </View>
         </Swiper>
         <View style={this.viewStyle()}>
-          <TitleText label={this.props.homes} />
+          <Text style={{ fontSize: 25, fontWeight: '600', color: '#4E4E4E'}}>Event Name {this.props.image}</Text>
+          <Image source={require("../../assets/images/cyclist.jpg")}
+            resizeMode = "stretch"
+            style={{flex:1, alignItems:'center', width:null, height:null, justifyContent:'center'}}>
+          </Image>
+          <View style={{flex: 1, padding: 20}}>
+            <Text style={{flex: 1, fontSize: 25, fontWeight: '600', color: '#4E4E4E'}}>Cycling Mt.Awesome {this.props.homes}</Text>
+          </View>
         </View>
       </Swiper>
 
