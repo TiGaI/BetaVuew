40759067bad5304af5cd3d7056d91670adc8083b
Junjie <junjiejiang94@gmail.com>
2017-03-14 22:59:54 -0700

    test

--- App/components/styles.js
+++ App/components/styles.js
@@ -12,5 +12,44 @@ export default StyleSheet.create({
 	toolbar: {
 		backgroundColor: '#E9EAED',
 		height: 56,
+	},
+	profileBox: {
+		backgroundColor: "blue",
+		flex: 2,
+		borderStyle: 'solid',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	socialStatus: {
+		flex: 3,
+		flexDirection: 'row',
+		marginTop: 10
+	},
+	innerbox: {
+		flex: 1,
+		flexDirection: 'column'
+	},
+	circle: {
+	height: 60, width: 60, borderRadius: 30,
+	alignItems: 'center',
+	justifyContent: 'center'
+	},
+	textInCicle:{
+		fontSize: 15,
+		color: 'white',
+		backgroundColor: "transparent",
+	},
+	profilecontainer: {
+		flex: 1,
+		width: 300,
+		flexDirection: 'row'
+	},
+	profilePic: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'center',
+		// position: "relative",
+		// bottom: 50,
+		// marginBottom: -50
 	}
 });
--- App/components/swiperView.js
+++ App/components/swiperView.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { AppRegistry, ScrollView, StyleSheet, Text, View, TextInput, TouchableOpacity, NavigatorIOS, ListView, Alert, AsyncStorage, Image } from 'react-native';
-import { Container, Content, Left, Body, Right, ListItem, Thumbnail } from 'native-base';
+import { AppRegistry, ScrollView, StyleSheet, View, TextInput, TouchableOpacity, NavigatorIOS, ListView, Alert, AsyncStorage, Image } from 'react-native';
+import { Container, Content, Left, Body, Right, Text, ListItem, Thumbnail, Card, CardItem, Button } from 'native-base';
 import Swiper from 'react-native-swiper'
 import randomcolor from 'randomcolor'
+import styles from './styles'
+
 
 class TitleText extends Component {
   render() {
@@ -21,7 +23,7 @@ class Swipe extends Component{
   }
   viewStyle() {
     return {
-      flex: 1,
+      flex: 2,
       backgroundColor: 'white',
       justifyContent: 'center',
       alignItems: 'center',
@@ -36,28 +38,57 @@ class Swipe extends Component{
         showsPagination={false}
         index={1}>
         <View style={this.viewStyle()}>
-          <Text style={{fontWeight: '300', fontSize: 30, marginTop: 20, color: 'black'}}>Profile</Text>
-          <ListItem avatar>
-                        <Left>
-                            <Thumbnail source={require("../../assets/images/cyclist.jpg")} />
-                        </Left>
-                        <Body>
-                            <Text style={{fontWeight: '300', fontSize: 18}}>Junjie Jiang</Text>
-                            <Text note style={{fontWeight: '100', fontSize: 15}}>Doing what you like will always keep you happy . .</Text>
-                        </Body>
-                        <Right>
-                        </Right>
-                    </ListItem>
-          <View style={{flex: 1, borderBottomColor: 'grey', borderBottomWidth: 1, borderStyle: 'solid'}}>
 
-            <Text style={{fontSize: 18, fontWeight: '500', color: '#4E4E4E', marginTop: 10}}>Event Name {this.props.name}</Text>
-            <Text style={{fontSize: 13, fontWeight: '200', marginTop: 20}}>Retro venmo seitan, la croix before they sold out cronut hell of paleo lomo
-            post-ironic organic readymade chillwave salvia try-hard. Mlkshk meditation chambray,
-             celiac pop-up letterpress art party sartorial put a bird on it meh prism crucifix disrupt kickstarter. </Text>
-          </View>
+          <Container>
+
+            <Content>
+              <View style={styles.profileBox}>
+                <Thumbnail style={{marginTop: 50, height: 80, width: 80, borderRadius: 40}} source={require("../../assets/images/cyclist.jpg")} />
+                <Text style={{textAlign: 'center', fontWeight: '300', fontSize: 25, marginTop: 10}}>Junjie Jiang</Text>
+                <Text style={{textAlign: 'center', fontWeight: '300', fontSize: 10, marginBottom: 10}}>Jupiter, Florida</Text>
+                <Text note style={{textAlign: 'center', fontWeight: '100', fontSize: 12, marginBottom: 10}}>You either end other career, or end your career</Text>
+
+                <View style={styles.socialStatus}>
+                  <View style={[styles.innerbox,  {alignItems: "flex-end"}]}>
+                    <View style={[styles.circle, {backgroundColor: '#54A78B'}]}>
+                      <Text style={styles.textInCicle, {color: 'white'}}>600</Text>
+                    </View>
+                    <Text style={{textAlign: 'center', color: "#FDFCFA", fontWeight: '300', fontSize: 10, marginBottom: 10, paddingRight: 10 }}>Followers</Text>
+                  </View>
+                  <View style={[styles.innerbox,  {alignItems: "center"}]}>
+                    <View style={[styles.circle, {backgroundColor: '#5B6AAB'}]}>
+                      <Text style={styles.textInCicle, {color: 'white'}}>27</Text>
+                    </View>
+                    <Text style={{textAlign: 'center', color: "#FDFCFA", fontWeight: '300', fontSize: 10, marginBottom: 10}}>Activities</Text>
+                  </View>
+                  <View style={[styles.innerbox,  {alignItems: "flex-start"}]}>
+                    <View style={[styles.circle, {backgroundColor: '#579BAB'}]}>
+                      <Text style={styles.textInCicle, {color: 'white'}}>8.7 K</Text>
+                    </View>
+                      <Text style={{textAlign: 'center', color: "#FDFCFA", fontWeight: '300', fontSize: 10, marginBottom: 10, paddingLeft: 2 }}>savage level</Text>
+                  </View>
+                </View>
+              </View>
+
+              <View style={styles.profilecontainer}>
+                </View>
 
 
+
+                <View style={{flex: 1, backgroundColor: "red", borderStyle: 'solid'}}>
+                  <Text style={{fontSize: 18, fontWeight: '500', color: '#4E4E4E', marginTop: 10}}>Event Name {this.props.name}</Text>
+                  <Text style={{fontSize: 13, fontWeight: '200', marginTop: 20}}>Retro venmo seitan, la croix before they sold out cronut hell of paleo lomo
+                  post-ironic organic readymade chillwave salvia try-hard. Mlkshk meditation chambray,
+                   celiac pop-up letterpress art party sartorial put a bird on it meh prism crucifix disrupt kickstarter. </Text>
+
+
+                </View>
+
+
+            </Content>
+          </Container>
         </View>
+
         <Swiper
           horizontal={false}
           loop={false}
