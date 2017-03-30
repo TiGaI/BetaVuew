599d91b0486c023de5b72e675873c3437e54df5c
git push -u origin <mazairaj94@gmail.com>
2017-03-30 10:32:50 -0700

    Hey

--- android/app/src/main/java/com/betavuew/MainApplication.java
+++ android/app/src/main/java/com/betavuew/MainApplication.java
@@ -3,6 +3,7 @@ package com.betavuew;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.airbnb.android.react.maps.MapsPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImagePickerPackage(),
             new FBSDKPackage(),
             new VectorIconsPackage(),
             new MapsPackage()
