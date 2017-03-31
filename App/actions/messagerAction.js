0876b542a81b08967a413d3cb27af65fa9381b3d
Junjie <junjiejiang94@gmail.com>
2017-03-30 17:30:49 -0700

    asd

--- App/actions/messagerAction.js
+++ App/actions/messagerAction.js
@@ -11,35 +11,35 @@ function doneFetching() {
   }
 }
 
-// export function getNewlyAddedFriend(currentUserID, friendToAddID) {
-//     return dispatch => {
-//         dispatch(fetching());
-//         console.log('currentUserID in getNewlyAddedFriend in messagerAction: ', currentUserID);
-//         console.log('friendToAddID in getNewlyAddedFriend in messagerAction: ', friendToAddID);
-//
-//         fetch('http://localhost:8080/getMessage', {
-//               method: 'POST',
-//               headers: {
-//                 'Accept': 'application/json',
-//                 'Content-Type': 'application/json'
-//               },
-//               body: JSON.stringify({
-//                 toUserID: currentUserID,
-//                 fromUserID: friendToAddID
-//               })
-//             }).then((response) => response.json())
-//             .then((responseJson) => {
-//
-//                 var userObject = [...responseJson];
-//                 console.log(userObject, "this is in getNewlyAddedFriend")
-//                 dispatch(getNewFriendComplete(userObject));
-//                 dispatch(doneFetching())
-//             })
-//             .catch((err) => {
-//               console.log('error: ', err)
-//             });
-//     };
-// }
+export function sendMessage(currentUserID, friendToAddID) {
+    return dispatch => {
+        dispatch(fetching());
+        console.log('currentUserID in getNewlyAddedFriend in messagerAction: ', currentUserID);
+        console.log('friendToAddID in getNewlyAddedFriend in messagerAction: ', friendToAddID);
+
+        fetch('http://localhost:8080/getMessage', {
+              method: 'POST',
+              headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({
+                toUserID: currentUserID,
+                fromUserID: friendToAddID
+              })
+            }).then((response) => response.json())
+            .then((responseJson) => {
+
+                var userObject = [...responseJson];
+                console.log(userObject, "this is in getNewlyAddedFriend")
+                dispatch(getNewFriendComplete(userObject));
+                dispatch(doneFetching())
+            })
+            .catch((err) => {
+              console.log('error: ', err)
+            });
+    };
+}
 
 export function getRecentlyAddedFriend(currentUserID) {
     return dispatch => {
