cb838274d2ed149f3ecca6a133a97c6968f5eb8a
Eric Abenojar <abenoeric@gmail.com>
2017-04-03 16:50:02 -0700

    updated message components

--- services/actionRoute.js
+++ services/actionRoute.js
@@ -1,172 +1,191 @@
 "use strict";
-var express = require('express');
+var express = require('express'),
+aws = require('aws-sdk'),
+bodyParser = require('body-parser'),
+multer = require('multer'),
+multerS3 = require('multer-s3');
 var router = express.Router();
 
 //model
 const User  = require('../models/models').User;
 const Activity= require('../models/models').Activity;
-const ActivityAction= require('../models/models').ActivityAction;
-const FriendRequest= require('../models/models').FriendRequest;
 
-const bodyParser = require('body-parser')
-router.use(bodyParser.urlencoded({ extended: true }));
-router.use(bodyParser.json())
-
-router.post('/sendFriendRequest', function(req, res){
-  console.log('req.body', req.body);
-
-  FriendRequest.find({$and: [
-          {toUser: req.body.toUser},
-          {fromUser: req.body.fromUser}]}, function(err, friendRequest) {
-      if (err) {
-          return {err, friendRequest}
-      }
-
-    console.log(friendRequest);
-
-    if(friendRequest.length === 0){
-      User.find({toUser: req.body.toUser}, function(err, user){
-        if(user){
-            console.log('adding a new friend')
-            var newFriend = new FriendRequest({
-              toUser: req.body.toUser,
-              fromUser: req.body.fromUser,
-              accepted: false
-            })
-            newFriend.save(function(err){
-              if (err) {
-                res.send(err)
-                console.log(err)
-              } else {
-                res.send('success')
-                console.log('Nice, you send a friend request.')
-              }
-            })
-        }else{
-          console.log("this user does not exist!");
-        }
-      });
-    }else{
-      console.log('you already send request to this friend exist!')
+//settting up S3
+var s3 = new aws.S3();
+var upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'newvuew',
+    metadata: function (req, file, cb) {
+      cb(null, {fieldName: file.fieldname});
+    },
+    Key: function (req, file, cb) {
+      // console.log('key', file);
+      cb(null, file.orginalname)
     }
   })
 });
 
-
-router.post('/getNotification', function(req, res){
-    FriendRequest.find({toUser: req.body.userID})
-     .sort({ createAt: -1})
-     .populate('fromUser', 'firstName lastName profileImg')
-     .select('fromUser').exec( function(err, friendRequests) {
-        if (err) {
-            return {err, friendRequests}
-        }
-
-      if(friendRequests){
-
-          console.log('hoping this is a array: ', friendRequests)
-
-             res.send(friendRequests)
-
-      }else{
-        res.send(null)
-        return null
-        console.log('No Friend Request notification')
+router.post('/getMyActivitiesInfo', function(req, res) {
+    var profile = req.body.userID
+    console.log("Got in here", req.body)
+    User.findById(profile)
+    .populate('activities', 'activityTitle activityImages timeStart timeEnd')
+    .exec(function(err, user) {
+      if (err) {
+          console.log("THIS IS AN ERROR! ", err)
+          return {err, user}
+      }
+      if (user) {
+        console.log("THIS IS THE USER BEFORE SEND: ", user)
+        res.send(user)
+        return user
+      } else {
+        console.log('fail in getMyActivitiesInfo! no user')
       }
     })
   });
 
-router.post('/acceptFriendRequest', function(req, res){
-  console.log(req.body);
-    FriendRequest.findOneAndRemove({$and: [
-        {toUser: req.body.toUserID},
-        {fromUser: req.body.fromUserID}]}, function(err, friendRequest) {
+
+router.post('/createActivity', upload.fields([{name: 'file', maxCount: 4},
+  { name: 'video', maxCount: 1}]), function(req, res){
+
+    var activity = req.body.activity;
+    console.log("this is the body: ", activity)
+    Activity.findOne({$and: [{activityTitle: req.body.activityTitle},
+      {activityCreator: activity.activityCreator}]}, function(err, activityfind) {
       if (err) {
-          return {err, friendRequest}
+                return {err, user}
       }
 
-      console.log(friendRequest);
+    if(!activityfind){
+      console.log("Creating an activity")
+      var newActivity = new Activity({
+        activityCreator: activity.activityCreator,
+        activityTitle: activity.activityTitle,
+        activityDescription: activity.activityDescription,
+        typeofRoom: activity.typeofRoom,
+        activityCategory: activity.activityCategory,
+        timeStart: activity.timeStart,
+        timeEnd: activity.timeEnd,
+        // activityImages: req.files['file'] ? req.files['file'][0].location : '',
+        // activityVideo: req.files['video'] ? req.files['video'][0].location : '',
+        activityLocation: activity.activityLocation,
+        interestUser: [],
+        activityCapacity: activity.activityCapacity
+      })
 
-    if(friendRequest){
+      newActivity.save(function(err, activityNew){
+        if (err) {
+          console.log('error has occur: ',  err)
+        } else {
+          console.log('Nice, you created a file')
+          console.log(activityNew);
+          User.findById(activityNew.activityCreator, function(err, user){
+            console.log(user)
+            user.activities = [...user.activities, ...[activityNew._id]]
+            user.save(function(err){
+              if (err) {
+                console.log('error has occur: ',  err)
+              } else {
+                console.log('Nice, activity added in the user model')
+              }
+            })
+          })
 
-      User.find({$or: [
-        {_id: req.body.toUserID},
-        {_id: req.body.fromUserID}]},function(err, users){
-        if(users){
-            console.log('users: ', users)
-
-
-            if(req.body.accepted){
-              friendRequest.accepted = req.body.accepted
-
-              console.log("I accepted a friend")
-              users.map(function(user, index){
-                console.log(user._id == req.body.toUserID)
-                if(user._id == req.body.toUserID){
-                  user.connections = user.connections.concat(req.body.fromUserID)
-                }else{
-                  user.connections = user.connections.concat(req.body.toUserID)
-                }
-                user.save(function(err){
-                  if (err) {
-                    console.log(err);
-                  } else {
-                    res.send(user)
-                    return user
-                  }
-                })
-              });
-            }else{
-              friendRequest.accepted = req.body.accepted
-              res.send(friendRequest)
-              console.log("I rejected a friend")
-            }
-        }else{
-          res.send('fail')
-          console.log("this user does not exist!");
         }
-      });
+      })
     }else{
-      res.send('fail')
-      console.log('you have not send a friend request him yet.')
+      console.log('activity already exist!')
+    }
+
+  })
+});
+
+//populate activities by category
+
+router.post('/populateActivities', function(req, res) {
+  console.log("INSIDE POPULATE ACTIVITIES")
+  console.log("CATEGORY", req.body.category)
+
+  var currActivities;
+  var prevActivities;
+  var nextActivities;
+  var activitiesObject = {}
+
+  Activity.find({activityCategory: req.body.category}).sort('-createdAt').populate({
+    path:'activityCreator',
+    options: {
+        limit: req.body.length,
+        sort: { created: -1},
     }
   })
+  .exec(function (err, articles) {
+      if (err) console.log('error is good');
+      var currArticles = articles
+      currActivities = [...currArticles]
+      activitiesObject["currCategory"] = currActivities
+      return activitiesObject
+    }).then((response) => {
+
+      Activity.find({activityCategory: req.body.prevCategory}).sort('-createdAt').populate({
+        path:'activityCreator',
+        options: {
+            limit: req.body.length,
+            sort: { created: -1},
+        }
+      }).exec(function (err, prevArticles) {
+
+          if (err) console.log('error is good');
+
+          prevActivities = [...prevArticles]
+          activitiesObject["prevCategory"] = prevActivities
+
+          return response;
+        }).then(() => {
+        Activity.find({activityCategory: req.body.nextCategory}).sort('-createdAt').populate({
+          path:'activityCreator',
+          options: {
+              limit: req.body.length,
+              sort: { created: -1},
+          }
+        }).exec(function (err, nextArticles) {
+            if (err) console.log('error is good');
+            nextActivities = [...nextArticles]
+            activitiesObject["nextCategory"] = nextActivities
+            return response
+          }).then(() => {
+              res.send(activitiesObject);
+          })
+        })
+      })
+    })
 
 
+// TODO: Edit an activity
+router.post('/editActivity', function(req, res) {
+    // req.body.id
+    Activity.findOneAndUpdate({_id: req.body.id}, req.body.data, function(err, doc){
+    if (err) return res.send(500, { error: err });
+    return res.send("succesfully saved");
+    });
 
 });
 
-router.post('/joinActivity', function(req, res){
+router.post('/getActivityOwner', function(req, res){
 
-    ActivityAction.find({$and: [{toUser: req.body.toUserID},
-      {fromUser: req.body.fromUserID}]}, function(err, friendRequest) {
-      if (err) {
-          return {err, friendRequest}
-      }
-    if(!friendRequest){
-      User.find({toUser: req.body.toUserID}, function(err, user){
-        if(user){
-            console('adding a new friend')
-            var newFriend = new ActivityAction({
-              toUser: req.body.toUserID,
-              fromUser: req.body.fromUserID,
-              accepted: false
-            })
-            newFriend.save(function(err){
-              if (err) {
-                res.send(err)
-              } else {
-                console.log('Nice, you send a friend request.')
-              }
-            })
-        }else{
-          console.log("this user does not exist!");
-        }
-      });
-    }else{
-      console.log('you already send request to this friend exist!')
-    }
-  })
+  User.findOne({_id: req.body.userID}, function(err, user) {
+          if (err) {
+              return {err, user}
+          }
+          if (user) {
+              return user
+          } else {
+            console.log("cannot find activity owner");
+            return null
+          }
+    });
+
 });
 
 
--- services/messageRoute.js
+++ services/messageRoute.js
@@ -8,25 +8,53 @@ const FriendRequest= require('../models/models').FriendRequest;
 const Message= require('../models/models').Message;
 
 router.post('/getMessage', function(req, res) {
+
+  console.log(req.body)
+
     Message.find({$or: [
-            { $and: [{toUser: req.body.toUserID}, {fromUser: req.body.fromUserID}]},
-            {$and: [{toUser: req.body.fromUserID}, {fromUser: req.body.toUserID}] } ]}
+            { $and: [{toUser: req.body.toUserID._id}, {fromUser: req.body.fromUserID}]},
+            {$and: [{toUser: req.body.fromUserID}, {fromUser: req.body.toUserID._id}] } ]}
           )
     .sort('-dateCreated')
     .exec(
-    function(err, user) {
+    function(err, messages) {
+      console.log(messages, 'this is the message')
             if (err) {
-                return {err, user}
+                return {err, messages}
             }
-            if (user) {
+            if (messages) {
 
-              return user
+              res.send(messages)
+              return JSON.stringify(messages)
             } else {
+              res.send(messages)
               console.log('fail in getMyActivitiesInfo! no user')
             }
         })
     }
 );
+
+router.post('/sendMessage', function(req, res) {
+  console.log(req.body, ' is the req.body of sendMessage in messageRoute')
+  var newMessage = new Message({
+      body: req.body.message[0].text,
+      toUser: req.body.toUserID,
+      fromUser: req.body.fromUserID,
+      dateCreated: new Date()
+  });
+  newMessage.save(function(err, message) {
+      if (err){
+        console.log(err);
+        return {err, message}
+      }
+      else{
+        res.send(message)
+        console.log('message sended!')
+        return {err, message}
+      }
+
+  });
+});
 
 router.post('/getNewlyAddedFriend', function(req, res) {
     console.log('INSIDE GETNEWFRIEND ROUTE', req.body.toUserID);
